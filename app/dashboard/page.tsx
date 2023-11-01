df83017fea8ce3c1ac8a54b4dfba847beae9a56f
Justin Padilla <justin.padilla@massvirtual.com>
2023-11-01 08:20:43 -0400

    starter

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+const DashboardPage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Dashboard Page!</h1>
+            <p>This is a starter Next.js page.</p>
+        </div>
+    );
+};
+
+export default DashboardPage;
